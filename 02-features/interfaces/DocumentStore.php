38cc5ec71cf7faa5b1b85bbc3258a93e28fd5e0e
Enjoy <kanneg881@gmail.com>
2018-04-15 00:01:22 +0800

    優化02-features/interfaces

--- 02-features/interfaces/DocumentStore.php
+++ 02-features/interfaces/DocumentStore.php
@@ -1,15 +1,32 @@
 <?php
+/**
+ * 實體文件
+ */
+
 class DocumentStore
 {
+    /** @var array 文字資料 */
     protected $data = [];
 
+    /**
+     * 增加文件
+     *
+     * @param Documentable $document
+     */
     public function addDocument(Documentable $document)
     {
+        /** @var string $key 文字資料的鍵值 */
         $key = $document->getId();
+        /** @var string $value 文字資料的值 */
         $value = $document->getContent();
         $this->data[$key] = $value;
     }
 
+    /**
+     * 取得文字資料
+     *
+     * @return array 文字資料
+     */
     public function getDocuments()
     {
         return $this->data;
