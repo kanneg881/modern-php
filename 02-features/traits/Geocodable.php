75ed61ec9ae63b578f07b3bb0b1422e3bfeb2b23
Enjoy <kanneg881@gmail.com>
2018-04-15 01:42:58 +0800

    優化02-features/traits

--- 02-features/traits/Geocodable.php
+++ 02-features/traits/Geocodable.php
@@ -1,25 +1,44 @@
 <?php
+/**
+ * 地理編碼
+ */
+
 trait Geocodable
 {
-    /** @var string */
+    /** @var string 地址 */
     protected $address;
 
-    /** @var \Geocoder\Geocoder */
+    /** @var \Geocoder\Geocoder 地理編碼器 */
     protected $geocoder;
 
-    /** @var \Geocoder\Model\AddressCollection */
+    /** @var \Geocoder\Model\AddressCollection 地理編碼結果 */
     protected $geocoderResult;
 
+    /**
+     * 設定地理編碼
+     *
+     * @param \Geocoder\Geocoder $geocoder 地理編碼器
+     */
     public function setGeocoder(\Geocoder\Geocoder $geocoder)
     {
         $this->geocoder = $geocoder;
     }
 
+    /**
+     * 設定地址
+     *
+     * @param string $address 地址
+     */
     public function setAddress($address)
     {
         $this->address = $address;
     }
 
+    /**
+     * 取得緯度
+     *
+     * @return float 緯度
+     */
     public function getLatitude()
     {
         if (!isset($this->geocoderResult)) {
@@ -29,6 +48,11 @@ trait Geocodable
         return $this->geocoderResult->first()->getLatitude();
     }
 
+    /**
+     * 取得經度
+     *
+     * @return float 經度
+     */
     public function getLongitude()
     {
         if (!isset($this->geocoderResult)) {
@@ -38,6 +62,11 @@ trait Geocodable
         return $this->geocoderResult->first()->getLongitude();
     }
 
+    /**
+     * 編碼地址
+     *
+     * @return bool
+     */
     protected function geocodeAddress()
     {
         $this->geocoderResult = $this->geocoder->geocode($this->address);
--- 02-features/traits/index.php
+++ 02-features/traits/index.php
@@ -3,14 +3,19 @@ require 'vendor/autoload.php';
 require 'Geocodable.php';
 require 'RetailStore.php';
 
+/** @var \Ivory\HttpAdapter\CurlHttpAdapter $adapter 適配器 */
 $adapter = new \Ivory\HttpAdapter\CurlHttpAdapter();
+/** @var \Geocoder\Provider\GoogleMaps $geocoder google 地圖 */
 $geocoder = new \Geocoder\Provider\GoogleMaps($adapter);
 
-$store = new RetailStore();
-$store->setAddress('420 9th Avenue, New York, NY 10001 USA');
-$store->setGeocoder($geocoder);
+/** @var RetailStore $retailStore 零售店 */
+$retailStore = new RetailStore();
+$retailStore->setAddress('420 9th Avenue, New York, NY 10001 USA');
+$retailStore->setGeocoder($geocoder);
 
-$latitude = $store->getLatitude();
-$longitude = $store->getLongitude();
+/** @var float $latitude 經度 */
+$latitude = $retailStore->getLatitude();
+/** @var float $longitude 緯度 */
+$longitude = $retailStore->getLongitude();
 
 echo $latitude, ':', $longitude;
