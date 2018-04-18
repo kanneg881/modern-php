0cf89580ce17e92eeeb8af1eb3753ad52f62b63b
Enjoy <kanneg881@gmail.com>
2018-04-18 09:39:02 +0800

    重構04-components/url-scanner-app

--- 04-components/url-scanner-app/scan.php
+++ 04-components/url-scanner-app/scan.php
@@ -1,23 +1,26 @@
 <?php
-// 1. Use Composer autoloader
+// 1. 使用 Composer 自動載入器
 require 'vendor/autoload.php';
 
-// 2. Instantiate Guzzle HTTP client
+// 2. 實例化 Guzzle HTTP 客戶端
 $client = new \GuzzleHttp\Client();
 
-// 3. Open and iterate CSV
+// 3. 開啟並迭代 CSV
 $csv = \League\Csv\Reader::createFromPath($argv[1]);
+
 foreach ($csv as $csvRow) {
     try {
-        // 4. Send HTTP OPTIONS request
-        $httpResponse = $client->options($csvRow[0]);
+        // 4. 發送 HTTP 請求
+        $httpResponse =  $client->request('GET', $csvRow[0]);
 
-        // 5. Inspect HTTP response status code
+        // 5. 查看 HTTP 回應狀態碼
         if ($httpResponse->getStatusCode() >= 400) {
             throw new \Exception();
         }
-    } catch (\Exception $e) {
-        // 6. Send bad URLs to standard out
+    } catch (\Exception $exception) {
+        // 6. 發送不良的URLs到標準輸出
         echo $csvRow[0] . PHP_EOL;
+    } catch (\GuzzleHttp\Exception\GuzzleException $exception) {
+        echo $exception->getMessage() . PHP_EOL;
     }
 }
