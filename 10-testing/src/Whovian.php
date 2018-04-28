f10fb1046f316ce67c60be562e476503eab1a5a2
Enjoy <kanneg881@gmail.com>
2018-04-29 03:13:52 +0800

    重構 10-testing

--- 10-testing/src/Whovian.php
+++ 10-testing/src/Whovian.php
@@ -1,14 +1,14 @@
 <?php
+
 class Whovian
 {
-    /**
-     * @var string
-     */
+    /** @var string 最愛的醫生 */
     protected $favoriteDoctor;
 
     /**
-     * Constructor
-     * @param  string $favoriteDoctor
+     * 建構子
+     *
+     * @param string $favoriteDoctor 最愛的醫生
      */
     public function __construct($favoriteDoctor)
     {
@@ -16,34 +16,37 @@ class Whovian
     }
 
     /**
-     * Say
-     * @return string
+     * 説
+     *
+     * @return string 回答最愛的醫生
      */
     public function say()
     {
-        return 'The best doctor is ' . $this->favoriteDoctor;
+        return '最好的醫生是' . $this->favoriteDoctor;
     }
 
     /**
-     * Respond to
-     * @param  string $input
-     * @return string
-     * @throws \Exception
+     * 回應
+     *
+     * @param  string $input 輸入醫生
+     * @return string 如果喜歡一樣的醫生就回答我認同
+     * @throws Exception 喜歡不一樣的醫生就拋出例外
      */
     public function respondTo($input)
     {
         $input = strtolower($input);
+        /** @var string $myDoctor 我喜歡的醫生 */
         $myDoctor = strtolower($this->favoriteDoctor);
 
         if (strpos($input, $myDoctor) === false) {
             throw new Exception(
                 sprintf(
-                    'No way! %s is the best doctor ever!',
+                    '沒門!%s是有史以來最好的醫生!',
                     $this->favoriteDoctor
                 )
             );
         }
 
-        return 'I agree!';
+        return '我認同!';
     }
 }
